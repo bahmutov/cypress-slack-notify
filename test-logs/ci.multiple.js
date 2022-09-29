102d50c61348aebe0109bbb25ca914449ab8edc7
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2022-09-29 08:34:57 -0400

    add checking ci multiple logs

---
+++ test-logs/ci.multiple.js
@@ -0,0 +1,36 @@
+const { expect } = require('chai')
+const { readFileSync } = require('fs')
+
+const logFilename = 'cypress-slack-notified.json'
+const ciJsonLog = readFileSync(logFilename, 'utf-8')
+const jsonLog = JSON.parse(ciJsonLog)
+
+const expectedLog = [
+  {
+    channel: '#cypress-slack-notify-multiple-sanity',
+    people: [],
+    sent: true,
+    runDashboardTags: ['sanity'],
+  },
+  {
+    channel: '#cypress-slack-notify-multiple-sanity',
+    people: [],
+    sent: true,
+    runDashboardTags: ['sanity'],
+  },
+  {
+    channel: '#cypress-slack-notify-multiple-sanity',
+    people: [],
+    sent: true,
+    runDashboardTags: ['sanity'],
+  },
+]
+
+jsonLog.forEach((record, k) => {
+  expect(record).to.have.property('runDashboardUrl')
+  delete record.runDashboardUrl
+})
+console.log('✅ each record has dashboard url')
+
+expect(jsonLog).to.deep.equal(expectedLog)
+console.log('✅ entire log')
